84450712fee42e1aff4a7d37463fadae4b24f333
cheb4 <a.splitas@gmail.com>
2019-09-23 21:46:50 +0300

    Updates

---
+++ precache-manifest.a695ba7de36fad33d27e9e229bde78c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6b2e2490bfce3b8c977513374fb5f2",
+    "url": "/nfq-uzduotis/index.html"
+  },
+  {
+    "revision": "126ee1021f40e0fa1348",
+    "url": "/nfq-uzduotis/static/css/main.7522e2bd.chunk.css"
+  },
+  {
+    "revision": "9ce831bd7e69c415472f",
+    "url": "/nfq-uzduotis/static/js/2.e25a698a.chunk.js"
+  },
+  {
+    "revision": "126ee1021f40e0fa1348",
+    "url": "/nfq-uzduotis/static/js/main.07f7904c.chunk.js"
+  },
+  {
+    "revision": "e025473c44eae150b316",
+    "url": "/nfq-uzduotis/static/js/runtime~main.f75fbf70.js"
+  }
+]);
